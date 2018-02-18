3d277e7bc92465b10b9ec9dd91453a213515e170
Administrator <administrator@mazzamac.local>
2018-02-18 15:14:59 -0300

    Fixed annotation on controller and created the unit tests

--- src/main/java/bookmarks/BookmarkRestController.java
+++ src/main/java/bookmarks/BookmarkRestController.java
@@ -2,15 +2,14 @@ package bookmarks;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.Collection;
 
+@RestController
+@RequestMapping("/{userId}/bookmarks")
 class BookmarkRestController {
 
     private final AccountRepository accountRepository;
